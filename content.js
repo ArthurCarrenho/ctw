3507efdf31df734af8c5aea3fec5c1f968d90739
ArthurCarrenho <50055205+ArthurCarrenho@users.noreply.github.com>
2019-08-20 17:56:20 -0300

    added CodeMirror to notranslate club

--- content.js
+++ content.js
@@ -8,5 +8,14 @@ var element2 = document.getElementById('discovery-resize-25')
 
 element2.classList.add('notranslate');
 
+var element3 = document.getElementsByClassName('CodeMirror');
+
+// Iterate through the retrieved elements and add the necessary class names.
+for(var i = 0; i < element3.length; i++)
+{
+    element3[i].classList.add('notranslate');
+    console.log(element3[i].className);
+}
+
 console.log('CTW: Finished')
 }, 1000);
