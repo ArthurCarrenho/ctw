4068dc3d46820b257b7525cf1834072cbaf1815c
ArthurCarrenho <50055205+ArthurCarrenho@users.noreply.github.com>
2019-08-20 17:45:14 -0300

    bug fixing && code improvments

--- content.js
+++ content.js
@@ -1,36 +1,12 @@
-var element = document.getElementsByClassName('editor-container');
-
-// Iterate through the retrieved elements and add the necessary class names.
-for(var i = 0; i < element.length; i++)
-{
-    element[i].classList.add('notranslate');
-    console.log(element[i].className);
-}
-var element2 = document.getElementsByClassName('CodeMirror');
-
-// Iterate through the retrieved elements and add the necessary class names.
-for(var i = 0; i < element2.length; i++)
-{
-    element2[i].classList.add('notranslate');
-    console.log(element2[i].className);
-}
 setTimeout(function(){ 
 // Get desired elements
-var element = document.getElementsByClassName('editor-container');
+var element = document.getElementById('discovery-resize-14');
 
-// Iterate through the retrieved elements and add the necessary class names.
-for(var i = 0; i < element.length; i++)
-{
-    element[i].classList.add('notranslate');
-    console.log(element[i].className);
-}
-var element2 = document.getElementsByClassName('CodeMirror');
+element.classList.add('notranslate');
 
-// Iterate through the retrieved elements and add the necessary class names.
-for(var i = 0; i < element2.length; i++)
-{
-    element2[i].classList.add('notranslate');
-    console.log(element2[i].className);
-}
+var element2 = document.getElementById('discovery-resize-25')
+
+element2.classList.add('notranslate');
+
 console.log('CTW: Finished')
-}, 5000);
+}, 1000);
